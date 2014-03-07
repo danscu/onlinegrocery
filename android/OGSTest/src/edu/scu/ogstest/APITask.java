65809e16beac990b8d2dc893c0687bd4e0f3ddf1
Danke <dankexie@hotmail.com>
2014-03-07 08:47:20 -0800

    Show READ/LIST operations
    The data from API are parsed into APIData (e.g., ShopperAccount) objects.
    No further JSON parsing is needed.

--- android/OGSTest/src/edu/scu/ogstest/APITask.java
+++ android/OGSTest/src/edu/scu/ogstest/APITask.java
@@ -2,113 +2,198 @@ package edu.scu.ogstest;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
-import com.google.gson.Gson;
+import org.apache.http.util.EntityUtils;
 
 import android.os.AsyncTask;
-import android.widget.TextView;
 
-public class APITask extends AsyncTask<String, Void, String> {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class APITask extends AsyncTask<Object, Void, APITask.APIResult> {
 	static protected final boolean USE_JSON = true;
 	
-	TextView textViewResult;
-	public APITask(TextView res) {
-		textViewResult = res;
+	public enum WhatToAccess {
+		/*
+		 * All we need is to add URLs and data Classes here.
+		 * This class will take care of JSON-ifying the data objects,
+		 * and talking to the API.
+		 */
+		SHOPPER_ACCOUNT("shopper_accounts", "shopper_account", ShopperAccount.class),
+		/* TODO Add other data tables here */
+		;
+		
+		private String url;			// Plural form
+		private String varName;		// Singular form
+		private Class<?> clazz;		// Data class
+		
+		WhatToAccess(String url, String varName, Class<?> clazz) {
+			this.url = url;
+			this.varName = varName;
+			this.clazz = clazz;
+		}
+		
+		public String getUrl() {
+			return url;
+		}
+		
+		public String getVarName() {
+			return varName;
+		}
+		
+		public Class<?> getDataClass() {
+			return clazz;
+		}
+	}
+	
+	public enum Operation {
+		LIST,
+		CREATE,
+		READ,
+		UPDATE,
+		DELETE,
+	}
+	
+	public enum APIErrorCode {
+		OK,
+		ERROR,
+	}
+	
+	public class APIResult {
+		APIErrorCode error;
+		Operation op;
+		String status;
+		String content;
+		List<? extends APIData> dataList;	// for list
+		APIData data;						// for read
+
+		public APIResult(APIErrorCode error, Operation op, String status, String content) {
+			this.error = error;
+			this.op = op;
+			this.status = status;
+			this.content = content;
+		}
+	}
+	
+	public interface Callback {
+		void onResultCallback(APITask from, APIResult result);
+	}
+	
+	protected WhatToAccess what;
+	protected Callback callback;
+	protected Gson gson = new Gson();
+	
+	/**
+	 * Access to database (CRUDL)
+	 * @param what		Which table to access
+	 * @param cb		Callback interface to receive API status and result
+	 */
+	public APITask(WhatToAccess what, Callback cb) {
+		this.what = what;
+		this.callback = cb;
+	}
+
+	public APITask create(APIData data) {
+		return (APITask) execute(Operation.CREATE, data);
+	}
+	
+	public APITask listData() {
+		return (APITask) execute(Operation.LIST);
+	}
+	
+	public APITask read(int id) {
+		return (APITask) execute(Operation.READ, null, id);
 	}
 	
 	@Override
-	protected String doInBackground(String... arg0) {
+	protected APIResult doInBackground(Object... args) {
+		Operation op = (Operation) args[0];
+		APIData data = args.length > 1 ? (APIData) args[1] : null;
+		int id = args.length > 2 ? (Integer)args[2] : -1;
+		
 		HttpClient httpclient = new DefaultHttpClient();
-		HttpPost httppost;
-		
-		if (USE_JSON) {
-			httppost = new HttpPost(Params.Host + "/shopper_accounts.json");
+		HttpRequestBase httpreq = null;
 
-			// JSON VERSION
-			ShopperAccount shopper_account = new ShopperAccount();
-			shopper_account.userName = "test_username_" + (new Random()).nextInt(100);
-			shopper_account.password = "1234";
-			shopper_account.firstName = "Tom";
-			shopper_account.lastName = "LastNameOfTom";
-			shopper_account.streetAddress = "n/a";
-			shopper_account.city = "n/a";
-			shopper_account.state = "n/a";
-			shopper_account.zipcode = "n/a";
-			shopper_account.phone = "n/a";
-			shopper_account.email = "n/a";
+		switch (op) {
+		case CREATE:
+			httpreq = new HttpPost(Params.Host + "/" + what.getUrl() + ".json");
+			break;
+		case LIST:
+			httpreq = new HttpGet(Params.Host + "/" + what.getUrl() + ".json");
+			break;
+		case READ:
+			httpreq = new HttpGet(Params.Host + "/" + what.getUrl() + "/" + id + ".json");
+		default:
+			break;
+		}
 
+		if (httpreq instanceof HttpPost) {
 			Map<String,Object> param = new HashMap<String,Object>();
-			param.put("shopper_account", shopper_account);
-
-			String json = new Gson().toJson(param);
-
+			param.put(what.getVarName(), data);
+	
+			String json = gson.toJson(param);
+	
 			try {
-				httppost.setEntity(new StringEntity(json));
+				((HttpPost)httpreq).setEntity(new StringEntity(json));
 			} catch (UnsupportedEncodingException e) {
 				e.printStackTrace();
 			}
-			httppost.setHeader("Content-type", "application/json");
-		} else {
-			httppost = new HttpPost(Params.Host + "/shopper_accounts");
-			
-			// URL ENCODING VERSION
-			HashMap<String,String> data = new HashMap<String,String>();
-			data.put("shopper_account[userName]",
-					"test_username_" + (new Random()).nextInt(100));
-			data.put("shopper_account[password]", "1234");
-			data.put("shopper_account[firstName]", "Tom");
-			data.put("shopper_account[lastName]", "LastNameOfTom");
-			data.put("shopper_account[streetAddress]", "n/a");
-			data.put("shopper_account[city]", "n/a");
-			data.put("shopper_account[state]", "n/a");
-			data.put("shopper_account[zipcode]", "n/a");
-			data.put("shopper_account[phone]", "n/a");
-			data.put("shopper_account[email]", "n/a");
-
-			List<NameValuePair> nvlist = new ArrayList<NameValuePair>();
-			for (Map.Entry<String,String> ent : data.entrySet()) {
-				nvlist.add(new BasicNameValuePair(ent.getKey(), ent.getValue()));
-			}
-			
-			try {
-				httppost.setEntity(new UrlEncodedFormEntity(nvlist));
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
-			}
-			httppost.setHeader("Content-type", "application/x-www-form-urlencoded");
+			httpreq.setHeader("Content-type", "application/json");
 		}
 		
-		String result = null;
+		String statusLine = "", content = "";
 		HttpResponse response = null;
+		APIErrorCode errCode;
 		try {
-			response = httpclient.execute(httppost);
-			result = response.getStatusLine().toString();
+			response = httpclient.execute(httpreq);
+			statusLine = response.getStatusLine().toString();
+			errCode = APIErrorCode.OK;		
+			content = EntityUtils.toString(response.getEntity(), "UTF-8");
 		} catch (ClientProtocolException e) {
-			result = e.toString();
+			statusLine = e.toString();
+			errCode = APIErrorCode.ERROR;
 		} catch (IOException e) {
-			result = e.toString();
+			statusLine = e.toString();
+			errCode = APIErrorCode.ERROR;
 		}
-		
-		return result;
+
+		return new APIResult(errCode, op, statusLine, content);
 	}
 
 	@Override
-	protected void onPostExecute(String result) {
-		textViewResult.setText(result);
+	protected void onPostExecute(APIResult result) {
+		if (callback == null)
+			return;
+
+		switch (result.op) {
+		case LIST:
+			switch (what) {
+			case SHOPPER_ACCOUNT:
+				Type listType = new TypeToken<List<ShopperAccount>>(){}.getType();
+				result.dataList = (List<ShopperAccount>) gson.fromJson(result.content, listType);
+				break;
+			}
+			break;
+		case READ:
+			APIData data = (APIData) gson.fromJson(result.content, what.getDataClass());
+			result.data = data;
+			break;
+		default:
+			break;
+		}
+
+		callback.onResultCallback(this, result);
 	}
 }
--- android/OGSTest/src/edu/scu/ogstest/ShopperAccount.java
+++ android/OGSTest/src/edu/scu/ogstest/ShopperAccount.java
@@ -1,6 +1,6 @@
 package edu.scu.ogstest;
 
-public class ShopperAccount {
+public class ShopperAccount extends APIData {
 	public String userName;
 	public String password;
 	public String firstName;
@@ -11,4 +11,13 @@ public class ShopperAccount {
 	public String zipcode;
 	public String phone;
 	public String email;
+	
+	@Override
+	public String toString() {
+		return "ShopperAccount [id = " + id + ", userName=" + userName + ", password="
+				+ password + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", streetAddress=" + streetAddress + ", city="
+				+ city + ", state=" + state + ", zipcode=" + zipcode
+				+ ", phone=" + phone + ", email=" + email + "]";
+	}
 }
