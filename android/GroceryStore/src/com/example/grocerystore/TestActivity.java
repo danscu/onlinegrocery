31429d080c88ddda12bf335af5a7d6863ad4463d
Danke <dankexie@hotmail.com>
2014-04-04 19:06:56 -0700

    Update TestActivity

--- android/GroceryStore/src/com/example/grocerystore/TestActivity.java
+++ android/GroceryStore/src/com/example/grocerystore/TestActivity.java
@@ -3,6 +3,7 @@ package com.example.grocerystore;
 import android.app.Activity;
 
 public class TestActivity extends Activity {
+	protected TextView textviewExample;
 
 	public TestActivity() {
 		// TODO Auto-generated constructor stub
