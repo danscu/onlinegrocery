ce86df99254050e2a386fc841a2f8ad335e60692
huizhuscu <hzhu@scu.edu>
2014-04-04 19:01:10 -0700

    Checkin a Test Activity
    Try GitHub Windows Client

---
+++ android/GroceryStore/src/com/example/grocerystore/TestActivity.java
@@ -0,0 +1,11 @@
+package com.example.grocerystore;
+
+import android.app.Activity;
+
+public class TestActivity extends Activity {
+
+	public TestActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
