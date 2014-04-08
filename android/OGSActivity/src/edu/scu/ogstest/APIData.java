df8a4da54e2e121ab02ea5392c1ff2c8281670fc
Danke Xie <danke.xie@gmail.com>
2014-04-08 08:12:27 -0700

    Remove OGSActivity

--- android/OGSActivity/src/edu/scu/ogstest/APIData.java
+++
@@ -1,8 +0,0 @@
-package edu.scu.ogstest;
-
-public class APIData {
-	public int id;
-
-	public APIData() {
-	}
-}
--- android/OGSActivity/src/edu/scu/ogstest/Params.java
+++
@@ -1,5 +0,0 @@
-package edu.scu.ogstest;
-
-public class Params {
-	public static final String Host = "http://54.193.104.59";
-}
